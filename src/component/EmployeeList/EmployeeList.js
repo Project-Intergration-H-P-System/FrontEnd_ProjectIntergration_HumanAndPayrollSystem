6968c2c99dd4b6564b0e7b64727130c30bae300d
nvh01022003 <nvh01022003@gmail.com>
2024-05-08 17:30:07 +0700

    big update

--- src/component/EmployeeList/EmployeeList.js
+++ src/component/EmployeeList/EmployeeList.js
@@ -1,36 +1,96 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import './EmployeeList.css';
-import AddEmployeeList from './AddEmployeeList';
+import Modal from 'react-modal';
+import { FaSearch } from "react-icons/fa";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
+
 function EmployeeTable() {
+    const [isOpen, setIsOpen] = useState(false);
     const [employees, setEmployees] = useState([]);
     const [loading, setLoading] = useState(true);
-    const [shareholderStatus, setShareholderStatus] = useState('');
-    const [shareholderStatuses, setShareholderStatuses] = useState([]);
-    const [searchResult, setSearchResult] = useState();
     const [searchTerm, setSearchTerm] = useState('');
-    const [gender, setGender] = useState('');
-    const [ethnicity, setEthnicity] = useState('');
-    const [employmentStatus, setEmploymentStatus] = useState('');
-    const [showAddForm, setShowAddForm] = useState(false);
+    const [originalEmployees, setOriginalEmployees] = useState([]);
+    const [name, setName] = useState('');
+
     const [newEmployee, setNewEmployee] = useState({
-        Name: '',
-        id: '',
-        Gender: '',
-        ethnic: '',
-        department: '',
-        VacationDay: '',
-        Benefit: ''
+        PERSONAL_ID: '',
+        CURRENT_FIRST_NAME: '',
+        CURRENT_MIDDLE_NAME: '',
+        CURRENT_LAST_NAME: '',
+        SOCIAL_SECURITY_NUMBER: '',
+        EmployeeNumber: '',
+        ETHNICITY: '',
+        SHAREHOLDER_STATUS: '',
+        CURRENT_GENDER: '',
+        PayRates_idPayRates: ''
     });
+    const handleChangee = (value) => {
+        setName(value);
+        handleSearch(value);
+    };
+
+    const handleChange = (e) => {
+        const { name, value } = e.target;
+        setNewEmployee({
+            ...newEmployee,
+            [name]: value
+        });
+    };
+
+
+
+    const handleAdd = async () => {
+        try {
+
+            if (!newEmployee.PERSONAL_ID || !newEmployee.CURRENT_FIRST_NAME || !newEmployee.CURRENT_LAST_NAME || !newEmployee.SOCIAL_SECURITY_NUMBER || !newEmployee.EmployeeNumber || !newEmployee.SHAREHOLDER_STATUS || !newEmployee.CURRENT_GENDER || !newEmployee.PayRates_idPayRates) {
+
+                alert('Vui lòng điền đầy đủ thông tin.');
+                return;
+            }
+
+
+            const existingEmployee = employees.find(employee => employee.PERSONAL_ID === newEmployee.PERSONAL_ID || employee.SOCIAL_SECURITY_NUMBER === newEmployee.SOCIAL_SECURITY_NUMBER || employee.PayRates_idPayRates === newEmployee.PayRates_idPayRates);
+            if (existingEmployee) {
+
+                alert('Personal ID, Social Security Number, hoặc ID Payrate đã tồn tại.');
+                return;
+            }
+            await axios.post('http://localhost:8080/create', newEmployee);
+            const response = await axios.get('http://localhost:8080/list');
+            setEmployees(response.data);
+
+
+            alert('Add new employee successfully !');
+            setIsOpen(false);
+            console.log(newEmployee);
+        } catch (error) {
+            console.error('Error saving data:', error);
+        }
+    };
+
+    const handleSearch = (searchTerm) => {
+        setSearchTerm(searchTerm);
+        if (typeof searchTerm === 'string') {
+            if (!searchTerm) {
+                setEmployees(originalEmployees);
+            } else {
+                const filteredEmployees = originalEmployees.filter(employee =>
+                    employee.FULLNAME.toLowerCase().includes(searchTerm.toLowerCase())
+                );
+                setEmployees(filteredEmployees);
+            }
+        }
+    };
+
     useEffect(() => {
         const fetchData = async () => {
             try {
                 const response = await axios.get('http://localhost:8080/list');
                 setEmployees(response.data);
+                setOriginalEmployees(response.data); // Lưu trữ danh sách nhân viên gốc
                 setLoading(false);
-
-                const uniqueStatuses = [...new Set(response.data.map(employee => employee.SHAREHOLDER_STATUS))];
-                setShareholderStatuses(uniqueStatuses);
             } catch (error) {
                 console.error('Error fetching data:', error);
             }
@@ -43,84 +103,25 @@ function EmployeeTable() {
         return <div>Loading...</div>;
     }
 
-
-    const handleSearch = () => {
-        const filtered = employees.filter(employee => {
-            return employee.SHAREHOLDER_STATUS === shareholderStatus;
-        });
-        setEmployees(filtered);
-    };
-    const handleChange = (e) => {
-        setSearchTerm(e.target.value);
+    const closeModal = () => {
+        setIsOpen(false);
     };
 
-    const handleShareholderStatusChange = (e) => {
-        setShareholderStatus(e.target.value);
-    };
-
-    const handleGenderChange = (e) => {
-        setGender(e.target.value);
-    };
-
-    const handleEthnicityChange = (e) => {
-        setEthnicity(e.target.value);
-    };
-
-    const handleEmploymentStatusChange = (e) => {
-        setEmploymentStatus(e.target.value);
-    };
-    const handleAdd = () => {
-        setShowAddForm(true);
-    };
-    const handleEdit = () => {
-        console.log('Sửa');
-    };
-
-    const handleDelete = () => {
-        console.log('Xóa');
-    };
-
-    const handleView = (item) => {
-        console.log('View', item);
-    };
-
-    const handleSave = () => {
-        const isAnyFieldNotEmpty = Object.values(newEmployee).some(value => value.trim() !== '');
-        const areAllFieldsNotEmpty = Object.values(newEmployee).every(value => value.trim() !== '');
-
-        if (areAllFieldsNotEmpty) {
-            const updatedData = [...searchResult, newEmployee];
-            setSearchResult(updatedData);
-            setShowAddForm(false);
-            setNewEmployee({
-                Name: '',
-                id: '',
-                Gender: '',
-                ethnic: '',
-                department: '',
-                VacationDay: '',
-                Benefit: ''
-            });
-        } else if (isAnyFieldNotEmpty) {
-            alert('Vui lòng điền đầy đủ thông tin');
-        } else {
-            setShowAddForm(false);
-        }
-    };
     return (
-        <div className="container_page">
-            <div className='Quantity'>
+        <div className="employee-list">
+            <div className='heading-page'>
                 <div className='loca-heading'>
                     <div className='heading'>
                         Employee List
                     </div>
                 </div>
-                <div className="search-container">
+                <div className="input-wrapper">
+                    <FaSearch id="search-icon" />
                     <input
-                        type="text"
-                        placeholder="Name"
-                        value={searchTerm}
-                        onChange={handleChange}
+                        placeholder="Type to search name..."
+                        value={name}
+                        onChange={(e) => handleChangee(e.target.value)}
+
                     />
                 </div>
             </div>
@@ -130,11 +131,13 @@ function EmployeeTable() {
                         <tr>
                             <th className="column-header">Full Name</th>
                             <th className="column-header">ID</th>
+                            <th className="column-header">Shareholder Status</th>
                             <th className="column-header">Gender</th>
                             <th className="column-header">Ethnicity</th>
-                            <th className="column-header">Shareholder Status</th>
+                            <th className="column-header">Country</th>
+                            <th className="column-header">Employment Status</th>
                             <th className="column-header">Vacation Day</th>
-                            <th className="column-header">SOCIAL SECURITY NUMBER</th>
+                            <th className="column-header">Action</th>
                         </tr>
                     </thead>
                     <tbody>
@@ -142,25 +145,169 @@ function EmployeeTable() {
                             <tr key={employee.id} className="table-row">
                                 <td className="table-cell">{employee.FULLNAME}</td>
                                 <td className="table-cell">{employee.PERSONAL_ID}</td>
+                                <td className="table-cell">{employee.SHAREHOLDER_STATUS}</td>
                                 <td className="table-cell">{employee.CURRENT_GENDER}</td>
                                 <td className="table-cell">{employee.ETHNICITY}</td>
-                                <td className="table-cell">{employee.SHAREHOLDER_STATUS}</td>
+                                <td className="table-cell">{employee.CURRENT_COUNTRY}</td>
+                                <td className="table-cell">{employee.EMPLOYMENT_STATUS}</td>
                                 <td className="table-cell">{employee.VacationDays}</td>
-                                <td className="table-cell">{employee.SOCIAL_SECURITY_NUMBER}</td>
-                                <button className="view-button" onClick={() => handleView(employee)}>View</button>
+                                <td>
+                                    <FontAwesomeIcon icon="fa-solid fa-list" />
+                                </td>
                             </tr>
                         ))}
                     </tbody>
                 </table>
             </div>
-            <div className="button-section">
-                <button className="add-button" onClick={handleAdd}>Thêm</button>
-                <button className="edit-button" onClick={handleEdit}>Sửa</button>
-                <button className="delete-button" onClick={handleDelete}>Xóa</button>
-            </div>
-            {showAddForm && (
-                <AddEmployeeList setShowAddForm={setShowAddForm} setEmployees={setEmployees} />
-            )}
+            <button
+                className="view-detail-button"
+                onClick={() => setIsOpen(true)}
+
+
+            >
+                NEW EMPLOYEE
+            </button>
+
+
+            <Modal
+                isOpen={isOpen}
+                onRequestClose={closeModal}
+                className="modal"
+                overlayClassName="modal-overlay"
+                appElement={document.getElementById('root')}
+            >
+                {newEmployee && (
+                    <div>
+                        <h2>Add new employee</h2>
+                        <div className="modal-row">
+                            <div className="modal-label">Personal ID:</div>
+                            <input
+                                type="number"
+                                name="PERSONAL_ID"
+                                value={newEmployee.PERSONAL_ID}
+                                onChange={handleChange}
+                                className="modal-input"
+                            />
+                        </div>
+                        <div className="modal-row">
+                            <div className="modal-label">First Name:</div>
+                            <input
+                                type="text"
+                                name="CURRENT_FIRST_NAME"
+                                value={newEmployee.CURRENT_FIRST_NAME}
+                                onChange={handleChange}
+                                className="modal-input"
+                            />
+                        </div>
+                        <div className="modal-row">
+                            <div className="modal-label">Middle Name:</div>
+                            <input
+                                type="text"
+                                name="CURRENT_MIDDLE_NAME"
+                                value={newEmployee.CURRENT_MIDDLE_NAME}
+                                onChange={handleChange}
+                                className="modal-input"
+                            />
+                        </div>
+                        <div className="modal-row">
+                            <div className="modal-label">Last Name:</div>
+                            <input
+                                type="text"
+                                name="CURRENT_LAST_NAME"
+                                value={newEmployee.CURRENT_LAST_NAME}
+                                onChange={handleChange}
+                                className="modal-input"
+                            />
+                        </div>
+                        <div className="modal-row">
+                            <div className="modal-label">Social Security Number:</div>
+                            <input
+                                type="number"
+                                name="SOCIAL_SECURITY_NUMBER"
+                                value={newEmployee.SOCIAL_SECURITY_NUMBER}
+                                onChange={handleChange}
+                                className="modal-input"
+                            />
+                        </div>
+                        <div className="modal-row">
+                            <div className="modal-label">Employee Number:</div>
+                            <input
+                                type="number"
+                                name="EmployeeNumber"
+                                value={newEmployee.EmployeeNumber}
+                                onChange={handleChange}
+                                className="modal-input"
+                            />
+                        </div>
+                        <div className="modal-row">
+                            <div className="modal-label">Ethnicity:</div>
+                            <input
+                                type="text"
+                                name="ETHNICITY"
+                                value={newEmployee.ETHNICITY}
+                                onChange={handleChange}
+                                className="modal-input"
+                            />
+                        </div>
+                        <div className="modal-row">
+                            <div className="modal-label">Shareholder Status:</div>
+                            <select
+                                name="SHAREHOLDER_STATUS"
+                                value={newEmployee.SHAREHOLDER_STATUS}
+                                onChange={handleChange}
+                                className="modal-input"
+                            >
+                                <option value="">Select</option>
+                                <option value="1">Yes</option>
+                                <option value="0">No</option>
+                            </select>
+                        </div>
+                        <div className="modal-row">
+                            <div className="modal-label">Gender:</div>
+                            <select
+                                name="CURRENT_GENDER"
+                                value={newEmployee.CURRENT_GENDER}
+                                onChange={handleChange}
+                                className="modal-input"
+                            >
+                                <option value="">Select</option>
+                                <option value="male">Male</option>
+                                <option value="female">Female</option>
+                            </select>
+                        </div>
+                        <div className="modal-row">
+                            <div className="modal-label">ID Payrates:</div>
+                            <select
+                                name="PayRates_idPayRates"
+                                value={newEmployee.PayRates_idPayRates}
+                                onChange={handleChange}
+                                className="modal-input"
+                            >
+                                <option value="">Select</option>
+                                <option value="11">11</option>
+                                <option value="22">22</option>
+                                <option value="33">33</option>
+                            </select>
+                        </div>
+                        <div className="modal-buttons">
+                            <button
+                                className="close-button"
+                                onClick={closeModal}
+                            >
+                                Cancel
+                            </button>
+                            <button
+                                className="save-button"
+                                onClick={handleAdd}
+                            >
+                                Add
+                            </button>
+
+                        </div>
+                    </div>
+                )
+                }
+            </Modal>
         </div>
     );
 }
